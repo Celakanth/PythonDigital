2be3ac5436cc1c489035d5d94ab53ec2bced212c
hoarec <hoarec@timeslive.co.za>
2017-10-07 12:52:57 +0100

    Added error exception

--- scrollingImages.py
+++ scrollingImages.py
@@ -8,35 +8,35 @@ def changeImage():
     ImageFile.LOAD_TRUNCATED_IMAGES = True
 
     global tkpi #need global so that the image does not get derefrenced out of function
-    #try:
-    #gets list of file names in certain directory. In this case, the directory it is in
-    dirlist = glob.glob("/Users/hoarec/Documents/Pictures/*.jpg")
+    try:
+        #gets list of file names in certain directory. In this case, the directory it is in
+        dirlist = glob.glob("/Users/hoarec/Documents/Pictures/*.jpg")
 
-    #get random image\
-    widths = root.winfo_width()
-    heights = root.winfo_height()
-    widths = widths - 10
-    heights = heights - 10
+        #get random image\
+        widths = root.winfo_width()
+        heights = root.winfo_height()
+        widths = widths - 10
+        heights = heights - 10
 
-    #randInt = random.randint(0, (len(dirlist) - 1))
-    filename = getfiles()
-    image = Image.open(filename)
-    image = image.resize((800,450), Image.NEAREST)
+        #randInt = random.randint(0, (len(dirlist) - 1))
+        filename = getfiles()
+        image = Image.open(filename)
+        image = image.resize((800,450), Image.NEAREST)
 
-    #set size to show, in this case the whole picture
-   # root.geometry('%dx%d' % (image.size[0],image.size[1]))
+        #set size to show, in this case the whole picture
+       # root.geometry('%dx%d' % (image.size[0],image.size[1]))
 
-    #Creates a Tkinter compatible photo image
-    tkpi = ImageTk.PhotoImage(image)
+        #Creates a Tkinter compatible photo image
+        tkpi = ImageTk.PhotoImage(image)
 
-    #Put image in a label and place it
-    label_image = Tkinter.Label(root, image=tkpi)
-    label_image.place(x=0,y=0,width=image.size[0],height=image.size[1])
+        #Put image in a label and place it
+        label_image = Tkinter.Label(root, image=tkpi)
+        label_image.place(x=0,y=0,width=image.size[0],height=image.size[1])
 
-    # call this function again in 1/2 a second
-    root.after(10000, changeImage)
-   # except ValueError:
-    #    print("Error")
+        # call this function again in 1/2 a second
+        root.after(10000, changeImage)
+    except ValueError:
+        print("Error")
 
 
 def getfiles():
